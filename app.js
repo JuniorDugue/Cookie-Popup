450207daa26e60cf80233c6f6bae76de6ce88a91
juniordugue <jrdugue@gmail.com>
2022-01-11 23:29:59 -0500

    added additional js animations to the svg cookie crumbs

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // gsap.to(".text", { y: 100, opacity: 0, duration: 1 });
-
 // gsap.fromTo(".text", { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 1 });
 
 const tl = gsap.timeline({ defaults: { duration: 0.75, ease: "power1.out" } });
@@ -7,9 +6,11 @@ const tl = gsap.timeline({ defaults: { duration: 0.75, ease: "power1.out" } });
 tl.fromTo(".cookie-container", { scale: 0 }, { scale: 1, ease: "elastic.out(1, 0.4)", duration: 1.5 });
 tl.fromTo(".cookie", { opacity: 0, x: -50, rotation: "-45deg" }, { opacity: 1, x: 0, rotation: "0" }, "<50%");
 tl.fromTo(".text", { x: 30, opacity: 0 }, { x: 0, opacity: 1 }, "<");
-// tl.fromTo(".cookie-title", { opacity: 0 }, { opacity: 1 });
-// tl.fromTo(".cookie-subtitle", { opacity: 0 }, { opacity: 1 });
 tl.fromTo("button", { opacity: 0 }, { opacity: 1 });
+
+// cookie jump
+tl.fromTo(".cookie", { y: 0, rotation: "0deg" }, { y: -20, rotation: "-20deg", yoyo: true, repeat: -1 });
+tl.fromTo("#crumbs", { y: 0 }, { y: -20, yoyo: true, repeat: -1 }, "<");
 
 // fading the cookie out
 const button = document.querySelector("button");
